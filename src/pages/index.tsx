48caaddb6a10a81b7c0364a9ff7750ff69b29bc9
Akhil Waghmare <akhilwaghmare@gmail.com>
2023-11-25 16:35:04 -0800

    Starter code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,3 @@
-import Card from "@/components/Card";
-import { animate, motion, useMotionValue } from "framer-motion";
-import { useEffect, useState } from "react";
-import useMeasure from "react-use-measure";
-
 export default function Home() {
   const images = [
     "/image-1.jpg",
@@ -14,62 +9,6 @@ export default function Home() {
     "/image-7.jpg",
     "/image-8.jpg",
   ];
-  const FAST_DURATION = 25;
-  const SLOW_DURATION = 75;
 
-  const [duration, setDuration] = useState(FAST_DURATION);
-  let [ref, { width }] = useMeasure();
-
-  const xTranslation = useMotionValue(0);
-
-  const [mustFinish, setMustFinish] = useState(false);
-  const [rerender, setRerender] = useState(false);
-
-  useEffect(() => {
-    let controls;
-    let finalPosition = -width / 2 - 8;
-
-    if (mustFinish) {
-      controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
-        ease: "linear",
-        duration: duration * (1 - xTranslation.get() / finalPosition),
-        onComplete: () => {
-          setMustFinish(false);
-          setRerender(!rerender);
-        },
-      });
-    } else {
-      controls = animate(xTranslation, [0, finalPosition], {
-        ease: "linear",
-        duration: duration,
-        repeat: Infinity,
-        repeatType: "loop",
-        repeatDelay: 0,
-      });
-    }
-
-    return controls?.stop;
-  }, [rerender, xTranslation, duration, width]);
-
-  return (
-    <main className="py-8">
-      <motion.div
-        className="absolute left-0 flex gap-4"
-        style={{ x: xTranslation }}
-        ref={ref}
-        onHoverStart={() => {
-          setMustFinish(true);
-          setDuration(SLOW_DURATION);
-        }}
-        onHoverEnd={() => {
-          setMustFinish(true);
-          setDuration(FAST_DURATION);
-        }}
-      >
-        {[...images, ...images].map((item, idx) => (
-          <Card image={`${item}`} key={idx} />
-        ))}
-      </motion.div>
-    </main>
-  );
+  return <main>Infinite carousel</main>;
 }
